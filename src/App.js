a4b06644bf256acfc63e942dd82d70322bdf2252
axel cochet <axel.cochet@edu.esiee.fr>
2021-06-16 17:43:12 +0200

    Ajout de la marge en dessous du block.

--- src/App.js
+++ src/App.js
@@ -52,7 +52,10 @@ class App extends Component {
             <Route exact path="/marketplace-details" component={MarketplaceDetails} />
             <Route exact path="/appsinstalled" component={AppsInstalled} />
             <Route exact path="/appsinstalled-details" component={AppsInstalledDetails} />
+            <div className="footer">
+            </div>
           </div>
+          
         ) : (
           <Redirect to="/auth" />
         )}
